cf9573fbb936ef07671887a9653c78aeef14938b
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-18 22:17:49 -0800

    Initial Commit

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+
+const Breadcrumbs = ({ crumbNameAndLinkArray }) => {
+  return <div></div>;
+};
+
+export default Breadcrumbs;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        // Define a custom font family for your header class
+        header: ['"nagel-variable"', "sans"],
+        // Define a custom font family for your body class
+        body: ['"cofo-sans-variable"', "sans"],
+      },
+      fontSize: {
+        38: "2.375rem", // 38px
+        32: "2rem", // 32px
+        24: "1.5rem", // 24px
+        14: "0.875rem", // 14px
+        12: "0.75rem", // 12px
+      },
+      fontWeight: {
+        400: 400,
+        450: 450,
+        500: 500,
+        700: 700,
       },
     },
   },
   plugins: [],
-}
+};
