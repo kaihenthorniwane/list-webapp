06252d637dbfe4367205505a467a0a998ea3bbe1
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-20 23:38:32 -0800

    Fixed key issue with folder components

--- src/components/Cards/FolderCard/FolderCard.jsx
+++ src/components/Cards/FolderCard/FolderCard.jsx
@@ -7,30 +7,35 @@ const FolderCard = ({ folder_id, folder_name }) => {
   // const folderDB = await sql `SELECT * FROM folders WHERE folder_id = ${folder_id}`;
   // const folderRow = folderDB.rows[0];
   const notes = [
-    <NoteCard
-      note_title={"Sample Note"}
-      note_content={"Sample Text"}
-      last_saved={"1 year"}
-      variant={"folder-view"}
-    />,
-    <NoteCard
-      note_title={"Sample Note"}
-      note_content={"Sample Text"}
-      last_saved={"1 year"}
-      variant={"folder-view"}
-    />,
-    <NoteCard
-      note_title={"Sample Note"}
-      note_content={"Sample Text"}
-      last_saved={"1 year"}
-      variant={"folder-view"}
-    />,
+    {
+      note_title: "Sample Note",
+      note_content: "Sample Text",
+      last_saved: "1 year",
+    },
+    {
+      note_title: "Sample Note",
+      note_content: "Sample Text",
+      last_saved: "1 year",
+    },
+    {
+      note_title: "Sample Note",
+      note_content: "Sample Text",
+      last_saved: "1 year",
+    },
   ];
   return (
     <div className="flex p-5 flex-col justify-around gap-1 relative h-folder-card">
       <FolderBack />
       <div className="flex absolute z-[-2] justify-evenly left-0 right-0 bottom-28">
-        {notes.map((note) => note)}
+        {notes.map((note, index) => (
+          <NoteCard
+            key={index}
+            note_title={note.note_title}
+            note_content={note.note_content}
+            last_saved={note.last_saved}
+            variant={"folder-view"}
+          />
+        ))}
       </div>
       <FolderFront />
       <span className="relative z-0 font-header text-24 font-600">
