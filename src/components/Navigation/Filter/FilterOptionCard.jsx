1453182c62d671a34fd4be16fa6ab65cfdf30907
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-25 00:35:04 -0700

    Improve switching animation

--- src/components/Navigation/Filter/FilterOptionCard.jsx
+++ src/components/Navigation/Filter/FilterOptionCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { brandedBezier } from "@/utils/animationConstants";
 
@@ -11,28 +11,42 @@ export default function FilterOptionCard({
   contextToGet,
 }) {
   const optionName = optionTitle.toString().toLowerCase();
+  const [iconColor, setIconColor] = useState("rgb(var(--Brand-Black))");
+
+  const textStyles = {
+    on: {
+      color: "rgb(var(--White))",
+      transition: {
+        delay: 0,
+        onComplete: () => {
+          setIconColor("rgb(var(--White))");
+        },
+      },
+    },
+    off: {
+      color: "rgb(var(--Brand-Black))",
+      transition: {
+        delay: 0.025,
+        onComplete: () => {
+          setIconColor("rgb(var(--Brand-Black))");
+        },
+      },
+    },
+  };
 
   return (
-    <div
-      className={
-        "w-full no-drag relative " +
-        (contextToGet === optionName ? "text-Brand-White" : "")
+    <motion.div
+      className={"w-full no-drag relative"}
+      animate={
+        contextToGet === optionName ? textStyles["on"] : textStyles["off"]
       }
       onClick={() => {
         contextToSet(optionName);
       }}
     >
       <div className="flex flex-col gap-2 relative z-[2]">
-        <div className="flex gap-1 items-center leading-none">
-          {IconComponent && (
-            <IconComponent
-              fill={
-                contextToGet === optionName
-                  ? "rgb(var(--White))"
-                  : "rgb(var(--Brand-Black))"
-              }
-            />
-          )}
+        <div className="flex gap-1 items-center leading-none ">
+          {IconComponent && <IconComponent fill={iconColor} />}
           <span className="-mb-1">{optionTitle}</span>
         </div>
         <div className="text-14 leading-none">{optionText}</div>
@@ -40,19 +54,19 @@ export default function FilterOptionCard({
       <AnimatePresence>
         {contextToGet === optionName && (
           <motion.div
-            initial={{ scale: 0.8 }}
+            initial={{ scale: 0.75 }}
             animate={{
               scale: 1,
-              transition: { duration: 0.5, ease: brandedBezier },
+              transition: { duration: 0.35, ease: brandedBezier },
             }}
             exit={{
-              scale: 0.8,
-              transition: { duration: 0.05, ease: "linear" },
+              scale: 0.9,
+              transition: { duration: 0.025, ease: "linear" },
             }}
             className="bg-Brand-Black absolute z-[1] rounded-xl -top-2.5 -bottom-2.5 -left-2.5 -right-2.5"
           />
         )}
       </AnimatePresence>
-    </div>
+    </motion.div>
   );
 }
