cc3d5d9294fa607c04d69f20c17649033e9a34fe
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 01:30:57 -0700

    Switch position of filter tabs to see whats going on with the svg shifting

--- src/components/Navigation/Filter/FilterBarMobile.jsx
+++ src/components/Navigation/Filter/FilterBarMobile.jsx
@@ -13,8 +13,8 @@ export default function FilterBarMobile() {
     <DropDownStateContext.Provider value={{ dropDownState, setDropDownState }}>
       <div className="sticky box-border mt-1 mb-10 top-0 w-full flex flex-col items-center z-10">
         <div className="no-drag w-full px-5 pt-4 max-w-4xl flex gap-4 bg-Brand-White relative z-[1]">
-          <FilterTab label={"Layout"} iconType={noteFormat} />
           <FilterTab label={"Order"} iconType={"Filter"} />
+          <FilterTab label={"Layout"} iconType={noteFormat} />
         </div>
         <AllFilterOptions />
         <div className="absolute top-full w-full h-10 bg-White-Gradient-Down"></div>
