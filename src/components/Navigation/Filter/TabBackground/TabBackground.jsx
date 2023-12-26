c457b81cb831cd2ceaa45f7d1afbb693083d2665
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 01:12:08 -0700

    adjust filter tab bacakground right margin

--- src/components/Navigation/Filter/TabBackground/TabBackground.jsx
+++ src/components/Navigation/Filter/TabBackground/TabBackground.jsx
@@ -5,7 +5,7 @@ import FolderTabRight from "./FolderTabRight";
 
 export default function TabBackground() {
   return (
-    <div className="absolute top-0 bottom-0 -mb-[3px] pb-[2px] -left-4 -right-4 flex items-end bg-white">
+    <div className="absolute top-0 bottom-0 -mb-[3px] pb-[2px] -left-4 -right-3 flex items-end bg-white">
       <FolderTabLeft />
       <FolderTabMid />
       <FolderTabRight />
