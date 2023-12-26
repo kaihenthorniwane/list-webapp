2ce39ba30c8a27048396adb9c6709e8789438121
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 11:39:17 -0700

    Add clip path to top and bottom of folder note background

--- src/components/Cards/NoteCard/FolderNoteBackgroundLayers/FolderNoteTop.jsx
+++ src/components/Cards/NoteCard/FolderNoteBackgroundLayers/FolderNoteTop.jsx
@@ -12,18 +12,30 @@ export default function FolderNoteTop() {
       }}
       className="w-full"
     >
-      <mask id="path-1-inside-1_292_9338" fill="white">
-        <path d="M168 25L0 25L1.11565e-06 -1.61897e-07L168 2.86102e-05L168 25Z" />
-      </mask>
-      <path
-        d="M168 25L0 25L1.11565e-06 -1.61897e-07L168 2.86102e-05L168 25Z"
-        fill="white"
-      />
-      <path
-        d="M1.07102e-06 1L168 1.00003L168 -0.999971L1.16027e-06 -1L1.07102e-06 1Z"
-        fill="#111A49"
-        mask="url(#path-1-inside-1_292_9338)"
-      />
+      <g clipPath="url(#clip0_292_9338)">
+        <mask id="path-1-inside-1_292_9338" fill="white">
+          <path d="M168 25L0 25L1.11565e-06 -1.61897e-07L168 2.86102e-05L168 25Z" />
+        </mask>
+        <path
+          d="M168 25L0 25L1.11565e-06 -1.61897e-07L168 2.86102e-05L168 25Z"
+          fill="white"
+        />
+        <path
+          d="M1.07102e-06 1L168 1.00003L168 -0.999971L1.16027e-06 -1L1.07102e-06 1Z"
+          fill="#111A49"
+          mask="url(#path-1-inside-1_292_9338)"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_292_9338">
+          <rect
+            width="168"
+            height="25"
+            fill="white"
+            transform="translate(168 25) rotate(-180)"
+          />
+        </clipPath>
+      </defs>
     </svg>
   );
 }
