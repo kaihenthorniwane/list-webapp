87acd71bb5fb6c03dd662ca26b109fa11db44569
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 17:21:44 -0700

    Update svg clipping in folder note bg

--- src/components/Cards/NoteCard/FolderNoteBackgroundLayers/FolderNoteBottom.jsx
+++ src/components/Cards/NoteCard/FolderNoteBackgroundLayers/FolderNoteBottom.jsx
@@ -2,7 +2,6 @@ export default function FolderNoteBottom() {
   return (
     <svg
       preserveAspectRatio="none"
-      viewBox="0 0 168 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       style={{
@@ -14,11 +13,11 @@ export default function FolderNoteBottom() {
     >
       <g clipPath="url(#clip0_376_10025)">
         <path d="M0 0H168V25H0V0Z" fill="white" />
-        <rect x="-0.340332" y="24" width="168.681" height="1" fill="#111A49" />
+        <rect x="-0.340332" y="24" width="600" height="1" fill="#111A49" />
       </g>
       <defs>
         <clipPath id="clip0_376_10025">
-          <rect width="168" height="25" fill="white" />
+          <rect width="600" height="25" fill="white" />
         </clipPath>
       </defs>
     </svg>
--- src/components/Cards/NoteCard/FolderNoteBackgroundLayers/FolderNoteTop.jsx
+++ src/components/Cards/NoteCard/FolderNoteBackgroundLayers/FolderNoteTop.jsx
@@ -1,7 +1,6 @@
 export default function FolderNoteTop() {
   return (
     <svg
-      viewBox="0 0 168 25"
       fill="none"
       preserveAspectRatio="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -15,9 +14,9 @@ export default function FolderNoteTop() {
       <g clipPath="url(#clip0_376_10020)">
         <path d="M0 0L168 0L168 25L3.16592e-06 25L0 0Z" fill="white" />
         <rect
-          x="168.34"
+          x="-50"
           y="1"
-          width="168.681"
+          width="600"
           height="1"
           transform="rotate(180 168.34 1)"
           fill="#111A49"
@@ -25,7 +24,7 @@ export default function FolderNoteTop() {
       </g>
       <defs>
         <clipPath id="clip0_376_10020">
-          <rect width="168" height="25" fill="white" />
+          <rect width="600" height="25" fill="white" />
         </clipPath>
       </defs>
     </svg>
