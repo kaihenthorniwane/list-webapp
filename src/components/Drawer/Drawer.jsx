ef429ca01a2588c7da0723cb9fa3c49fc8ee500b
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 20:01:02 -0700

    Disable scrolling when drawer open, with fixed

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -7,7 +7,7 @@ export default function Drawer({ isOpen, children, setContext }) {
       {isOpen && (
         <>
           <motion.div
-            className="absolute z-[100] left-0 right-0 bottom-0 px-5 pb-5 pt-4  bg-Brand-White flex flex-col items-stretch gap-6 rounded-tl-2xl rounded-tr-2xl"
+            className="fixed z-[100] left-0 right-0 bottom-0 px-5 pb-5 pt-4  bg-Brand-White flex flex-col items-stretch gap-6 rounded-tl-2xl rounded-tr-2xl"
             initial={{ translateY: "100%" }}
             animate={{ translateY: 0 }}
             exit={{ translateY: "100%" }}
@@ -26,7 +26,7 @@ export default function Drawer({ isOpen, children, setContext }) {
             onClick={() => {
               setContext(false);
             }}
-            className="absolute z-[99] w-full h-full bg-Brand-Black opacity-50"
+            className="fixed z-[99] w-full h-full bg-Brand-Black opacity-50"
           />
         </>
       )}
