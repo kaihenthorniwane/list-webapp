042f22c63c5f8b5c14e187ed62d8b38d74cba1ca
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-27 00:23:07 -0700

    Add press states for drawer options

--- src/components/Drawer/Drawer Contents/Drawer Components/DrawerOption.jsx
+++ src/components/Drawer/Drawer Contents/Drawer Components/DrawerOption.jsx
@@ -4,7 +4,10 @@ export default function DrawerOption({
   color = "rgb(var(--Brand-Black))",
 }) {
   return (
-    <div className="flex gap-2.5 items-center" style={{ color: color }}>
+    <div
+      className="flex gap-2.5 items-center brand-button-transition lesser-transform opacity-button-transition"
+      style={{ color: color }}
+    >
       <img src={iconSrc} />
       <span className="mt-1">{text}</span>
     </div>
