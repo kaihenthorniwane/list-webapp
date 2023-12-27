fb8f83ea87efd75205690161a0c9866cc4d345c6
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 20:24:19 -0700

    Set body overflow to hidden when drawer appears

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -2,12 +2,21 @@ import { brandedBezier } from "@/utils/animationConstants";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function Drawer({ isOpen, children, setContext }) {
+  const handleAnimationStart = () => {
+    // Action when the animation starts
+    document.body.style.overflow = "hidden";
+  };
+
+  const handleExitComplete = () => {
+    // Action when the exit animation ends
+    document.body.style.overflow = "auto";
+  };
   return (
     <AnimatePresence>
       {isOpen && (
         <>
           <motion.div
-            className="fixed z-[100] left-0 right-0 bottom-0 px-5 pb-5 pt-4  bg-Brand-White flex flex-col items-stretch gap-6 rounded-tl-2xl rounded-tr-2xl"
+            className="fixed z-[100] left-0 right-0 bottom-0 px-5 pb-5 pt-4  bg-Brand-White flex flex-col items-stretch gap-6 rounded-tl-[2rem] rounded-tr-[2rem]"
             initial={{ translateY: "100%" }}
             animate={{ translateY: 0 }}
             exit={{ translateY: "100%" }}
@@ -21,11 +30,16 @@ export default function Drawer({ isOpen, children, setContext }) {
             animate={{ opacity: 0.5, transition: { ease: brandedBezier } }}
             exit={{
               opacity: 0,
-              transition: { duration: 0.025, ease: "linear" },
+              transition: {
+                duration: 0.025,
+                ease: "linear",
+                onComplete: handleExitComplete,
+              },
             }}
             onClick={() => {
               setContext(false);
             }}
+            onAnimationStart={handleAnimationStart}
             className="fixed z-[99] w-full h-full bg-Brand-Black opacity-50"
           />
         </>
