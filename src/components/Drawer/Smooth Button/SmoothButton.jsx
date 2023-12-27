ba602a2aa613d8fdb5b153d88a415ea44801ab41
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-26 23:25:35 -0700

    Add cancel button push state

--- src/components/Drawer/Smooth Button/SmoothButton.jsx
+++ src/components/Drawer/Smooth Button/SmoothButton.jsx
@@ -7,12 +7,12 @@ export default function SmoothButton({
 }) {
   return (
     <div
-      className="relative w-full"
+      className="relative w-full brand-button-transition"
       onClick={() => {
         setContext(valueToSet);
       }}
     >
-      <div className="absolute mt-0.5 bottom-0 top-0 left-0 right-0 flex justify-center items-center">
+      <div className="absolute z-[1] mt-0.5 bottom-0 top-0 left-0 right-0 flex justify-center items-center">
         {text}
       </div>
       <SmoothButtonBackground />
