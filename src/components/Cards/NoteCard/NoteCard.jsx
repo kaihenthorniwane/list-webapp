866ac99fe95c0e20f46f8931d6137156546058f5
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2023-12-29 13:59:35 -0800

    Try to fix safari flexbox issue again

--- src/components/Cards/NoteCard/NoteCard.jsx
+++ src/components/Cards/NoteCard/NoteCard.jsx
@@ -134,9 +134,7 @@ const NoteCard = ({
     >
       <div className={"relative z-1 " + wrapperVariantStyles[currentVariant]}>
         <div className="flex leading-tight justify-between gap-2 ">
-          <span
-            className={"w-full shrink " + titleVariantStyles[currentVariant]}
-          >
+          <span className={"grow " + titleVariantStyles[currentVariant]}>
             {note_title}
           </span>
           {variant === "writer" && (
