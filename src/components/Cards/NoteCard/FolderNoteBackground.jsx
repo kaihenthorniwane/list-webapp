577f447e633053a4dec3ad7ae5b2424cce49588f
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-01-15 23:57:23 -0800

    Add Rounded white background to folder note background

--- src/components/Cards/NoteCard/FolderNoteBackground.jsx
+++ src/components/Cards/NoteCard/FolderNoteBackground.jsx
@@ -20,7 +20,7 @@ const FolderNoteBackground = () => {
         width: "100%", // or any specific width
         height: "100%", // or any specific height
       }}
-      className="absolute left-0 top-0 bottom-0 right-0 z-[-1] no-drag"
+      className="absolute left-0 top-0 bottom-0 right-0 z-[-1] no-drag bg-Brand-White rounded-3xl"
     >
       {/* Corners */}
       <FolderNoteCornerTopLeft />
